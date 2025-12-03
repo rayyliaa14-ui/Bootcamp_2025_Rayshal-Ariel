28ac1d91e1dfeb5c481e74b9e72aa29bbd05f93f
rayshal <rayyliaa14@gmail.com>
2025-12-03 11:29:36 +0700

    ngerapihin

--- Node_JS/bio.js
+++ Node_JS/bio.js
@@ -5,20 +5,20 @@ const rl = readline.createInterface({
 })
 
 rl.question("What is your name? ", (name) => {
-    rl.question("People call you who? ", (called) =>{
-        rl.question("Where and when is your born? ", (ttgl) =>{
-            rl.question("What is your gender? ", (gender) => {
-                rl.question("What is ur phone number? ", (phone) => {
-                    rl.question("What is your email? ", (email) => {
-                        rl.question("Where do u live? ", (address) =>{
-                            console.log("==== your data ====");
-                            console.log(`your name is : ${name}
-                                \npeople call you : ${called}
-                                \nyour birth date and place is : ${ttgl}
-                                \nyour gender is :  ${gender}
-                                \nyour phone number is : ${phone}
-                                \nyour email is : ${email}
-                                \nyou live in : ${address}`);
+rl.question("People call you who? ", (called) =>{
+rl.question("Where and when is your born? ", (ttgl) =>{
+rl.question("What is your gender? ", (gender) => {
+rl.question("What is ur phone number? ", (phone) => {
+rl.question("What is your email? ", (email) => {
+rl.question("Where do u live? ", (address) =>{
+console.log("\n==== your data ====\n");
+console.log(`your name is : ${name}
+\npeople call you : ${called}
+\nyour birth date and place is : ${ttgl}
+\nyour gender is :  ${gender}
+\nyour phone number is : ${phone}
+\nyour email is : ${email}
+\nyou live in : ${address}`);
                         })
                     })
                 })
