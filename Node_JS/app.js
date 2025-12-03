3b31ed4d083783918c8ddb78a70675f38a880555
rayshal <rayyliaa14@gmail.com>
2025-12-03 11:23:07 +0700

    add biodata file with node js

--- Node_JS/app.js
+++ Node_JS/app.js
@@ -1,6 +1,22 @@
-const fs = require('fs');
- console.log(fs);
+// const fs = require('fs');
+// const { stdin, stdout } = require('process');
+//  console.log(fs);
 
 //  create file with synchronous 
 
-fs.writeFileSync("test.txt","Hello World by synchronous");
+// fs.writeFileSync("test.txt","Hello World by synchronous!!");
+
+// fs.readFile('test.txt','utf-8', (err,data) => {
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+const readline = require('readline');
+    const rls = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    rls.question("What is your name? ", (name) =>{
+        console.log(`Hi, ${name}!`);
+        rls.close();
+    });
---
+++ Node_JS/bio.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("What is your name? ", (name) => {
+    rl.question("People call you who? ", (called) =>{
+        rl.question("Where and when is your born? ", (ttgl) =>{
+            rl.question("What is your gender? ", (gender) => {
+                rl.question("What is ur phone number? ", (phone) => {
+                    rl.question("What is your email? ", (email) => {
+                        rl.question("Where do u live? ", (address) =>{
+                            console.log("==== your data ====");
+                            console.log(`your name is : ${name}
+                                \npeople call you : ${called}
+                                \nyour birth date and place is : ${ttgl}
+                                \nyour gender is :  ${gender}
+                                \nyour phone number is : ${phone}
+                                \nyour email is : ${email}
+                                \nyou live in : ${address}`);
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
