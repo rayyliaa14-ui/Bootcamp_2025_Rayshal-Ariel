f48ec51519f80286a1f32f62da8ef086bb19c95d
rayshal <rayyliaa14@gmail.com>
2025-12-04 23:19:39 +0700

    Update Homework -[Import]

--- NPM/apps.js
+++ NPM/apps.js
@@ -1,22 +1,9 @@
 //Deklarasi variable 
 const fs = require('fs');
-const readline = require('readline');
-const { Readline } = require('readline/promises');
 
-// make a function to ask
-const question = (ask) =>{
-    return new Promise((resolve,reject)=>{
-        rl.question(ask,(inputVariable)=>{
-            resolve(inputVariable);
-        })
-    })
-}
+const Readline = require('readline/promises');
+const { main } = require('./contact.js');
 
-//Dekalari rl sebagai I/O
-const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-})
 
 //Apakah Folder dan File sudah ada?
 const dPath = './data';
@@ -25,24 +12,10 @@ if(!fs.existsSync(dPath)){
 }
 
 //cek apakah sudah ada file?
-const dataPath = './data/contacs.json';
+const dataPath = './data/contact.json';
 if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath,'[]','utf-8');
-
 } 
-//Proses input data
-const main = async() => {
-    const name = await question('What is your name? ');
-    const phone = await question('What is your phone number? ');
-    const email = await question('What is your email? ');
 
-    const contact = {name,phone,email};
-    const file = fs.readFileSync('./data/contacs.json','utf8');
-    const contacts = JSON.parse(file);
-    contacts.push(contact);
-    fs.writeFileSync('./data/contacs.json',JSON.stringify(contacts));
-        console.log("Thanks for input the data!");
-        rl.close();
-    }
 
-    main()
+    main();
