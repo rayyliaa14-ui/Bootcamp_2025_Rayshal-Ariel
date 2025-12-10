7abd140ecebde48d01e30d1b488f35bf39600929
rayshal <rayyliaa14@gmail.com>
2025-12-10 11:07:12 +0700

    update batch file 10 Dec

--- WebServer/app.js
+++ WebServer/app.js
@@ -3,22 +3,47 @@ const app = express()
 const port = 3000
 const fs = require('fs');
 
+//information about ejs
+app.set('view engine','ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.sendFile('./index.html',{root: __dirname})
-    //   res.send('Hello World!');
+  const contact = [{
+    name:'ray',
+    email:'ray@gmail.com',
+  },{
+    name:'sal',
+    email:'sal@yahoo.com',
+  },{
+    name:'ariel',
+    email:'ariel@try.com',
+  }];
+  res.render('index', {name: "rayshal", title: "Webserver EJS",contact});
 })
 app.get('/contact', (req, res) => {
-    res.sendFile('./contact.html',{root: __dirname});
+  res.render('contact');
 })
 app.get('/about', (req, res) => {
-  res.sendFile('./about.html', {root: __dirname});
+  res.render('about')
+  // res.sendFile('./views/about.html', {root: __dirname});
 })
+
+app.get('/produk/:id', (req, res) => {
+    res.send(` product ID = ${req.params.id} 
+      <br> category = ${req.query.category}`);
+});
 
 app.use('/',(req,res) =>{
     res.status(404);
     res.send('page not found');
 })
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port [${port}]`)
 })
+
+
+
+
 
